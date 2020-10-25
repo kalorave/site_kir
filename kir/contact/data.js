70e20d01a9d309ccd726ce03379db8bd6313700b
JoeBair <workpage.maxim@gmail.com>
2020-10-26 01:48:55 +0300

    fix: fixed references

--- kir/contact/data.js
+++ kir/contact/data.js
@@ -7,40 +7,55 @@
   // person cards
   cards: [
     {
-      logo: "logo.png",
-      name: "Кирил",
+      logo: "Kirill.jpg",
+      name: "Кирилл",
       position: "руководитель проекта",
       contacts: {
-        email: "mapic-russia@reedexpo.ru",
+        email: "kir@aaex.ru",
       }
     },
     {
-      logo: "logo.png",
-      name: "Кирил",
-      position: "руководитель проекта",
+      logo: "Natalia.jpg",
+      name: "Наталия",
+      position: "генеральный директор",
       contacts: {
-        email: "mapic-russia@reedexpo.ru",
+        email: "titan@aaex.ru",
       }
     },
     {
-      logo: "logo.png",
-      name: "Кирил",
-      position: "руководитель проекта",
+      logo: "Liza.jpg",
+      name: "Елизавета",
+      position: "главный редактор",
       contacts: {
-        email: "mapic-russia@reedexpo.ru",
+        email: "liza@aaex.ru",
       }
     },
     {
-      logo: "logo.png",
+      logo: "Yulia.jpg",
+      name: "Юлия",
+      position: "выпускающий редактор",
+      contacts: {
+        email: "juliya@aaex.ru",
+      }
+    },
+    {
+      logo: "Nelly.jpg",
+      name: "Нелли",
+      position: "художественный редактор",
+      contacts: {
+        email: "cb@aaex.ru",
+      }
+    },
+    {
+      logo: "/img/subscribe.jpg",
       address: "196006, г. Санкт-Петербург, Лиговский пр., 274, оф. 119",
       info: {
         regСertificate: {
-          text: 'Регистрационное свидетельство ПИ №77-5837',
-          link: '#'
+          text: 'Регистрационное свидетельство ПИ №77-5837'
         },
         conditionsOfUse: {
           text: 'Условия перепечатки материалов сайта',
-          link: '#'
+          link: '/pdf/terms.pdf'
         }
       },
       contacts: {
@@ -49,10 +64,10 @@
           link: '+78123363867'
         },
         social: [
-          {icon: 'youtube.png', link: '#'},
-          {icon: 'facebook.png', link: '#'},
-          {icon: 'twitter.png', link: '#'},
-          {icon: 'instagram.png', link: '#'}
+          {icon: '/img/sni_01.svg', link: 'https://www.youtube.com/channel/UCuHoBMbxTBX2fZqhhOBxqhw'},
+          {icon: '/img/sni_02.svg', link: 'https://www.facebook.com/zurnal.industriirazvlecenij'},
+          {icon: '/img/sni_03.svg', link: 'https://twitter.com/kir_aaex'},
+          {icon: '/img/sni_04.svg', link: 'https://www.instagram.com/kir.aaex.ru/'}
         ]
       }
     }
